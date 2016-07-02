62c366ff3bf41ac8c14bb3d1ea0825dc1aeb32bd
athanzhang <athan1114@gmail.com>
2016-07-02 16:05:25 +0800

    Finishing the pictures and text replacement

--- Java_DOM/Chapter4/scripts/showPic.js
+++ Java_DOM/Chapter4/scripts/showPic.js
@@ -2,4 +2,10 @@ function showPic(whichPic) {
 	var source = whichPic.getAttribute("href");
 	var placeholder=document.getElementById("placeholder");
 	placeholder.setAttribute("src",source);
+	//获取节点title值
+	var title = whichPic.getAttribute("title");
+	//获取相应ID对象
+	var description= document.getElementById("description");
+	//替换相应对象值
+	description.firstChild.nodeValue=title;
 }
